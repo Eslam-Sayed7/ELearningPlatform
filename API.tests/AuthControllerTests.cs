19f3323ef68ac246ef6a465bcbd1b8536169c2c0
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2025-06-26 16:43:39 +0300

    refact-unit-tests

--- API.tests/AuthControllerTests.cs
+++ API.tests/AuthControllerTests.cs
@@ -7,6 +7,8 @@ using Infrastructure.Data;
 using Infrastructure.Data.Models;
 using Infrastructure.Data.Services;
 using Infrastructure.Dtos;
+using Microsoft.AspNetCore.Http;
+using Infrastructure.Dtos;
 
 namespace API.Tests
 {
@@ -21,6 +23,13 @@ namespace API.Tests
             _mockAuthService = new Mock<IAuthService>();
             _mockStudentService = new Mock<IStudentService>();
             _authController = new AuthController(_mockAuthService.Object, _mockStudentService.Object);
+            
+            // Mock HttpContext for cookie operations
+            var httpContext = new DefaultHttpContext();
+            _authController.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };        
         }
 
         [Fact]
@@ -35,7 +44,12 @@ namespace API.Tests
                 Email = "admin@gmail.com",
                 Password = "Pa$$w0rd"
             };
-            var authModel = new AuthModel { IsAuthenticated = true, User = new AppUser() };
+            var authModel = new AuthModel {
+                IsAuthenticated = true, 
+                User = new AppUser(),
+                RefreshToken = "dummy-token",
+                RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7)
+            };
             _mockAuthService.Setup(service => service.RegisterUserAsync(registerModel)).ReturnsAsync(authModel);
             _mockStudentService.Setup(service => service.CreateStudentAsync(It.IsAny<Student>())).ReturnsAsync(new Student());
 
@@ -44,7 +58,7 @@ namespace API.Tests
 
             // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
-            var returnValue = Assert.IsType<AuthModel>(okResult.Value);
+            var returnValue = Assert.IsType<LoginResponseDto>(okResult.Value);
             Assert.True(returnValue.IsAuthenticated);
         }
 
